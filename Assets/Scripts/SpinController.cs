00bdb910a644e169a6769eb5e67b3fc056bcef34
onursercanyilmaz <onursercanyilmaz@gmail.com>
2023-05-21 23:17:16 +0300

    spin movement applied

---
+++ Assets/Scripts/SpinController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class SpinController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+        rb.AddForce(movement * moveSpeed);
+    }
+}
